9e4f80cec1710fc36d4431aec6df03dcd4867043
Lauri Haara <lauri.haara@gmail.com>
2018-11-19 17:40:09 +0200

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,15 +1,37 @@
 const dummy = (blogs) => {
   return 1
 }
-  
-
 
 const totalLikes = (blogs) => {
     const allLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0)    
     return allLikes
 }
 
+const favouriteBlog = (blogs) => {
+    const formatBlog = (blog) => {
+        return {
+          title: blog.title,
+          author: blog.author,
+          likes: blog.likes
+        }
+    }
+    const mostLikes = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current, [])    
+    //console.log(formatBlog(mostLikes))
+    return formatBlog(mostLikes) 
+
+    //jos halutaan tyhjstä taulukosta lähettää vastauksena tyhjä, eikä "undefined"
+/*     if (mostLikes === undefined || mostLikes.length == 0){
+      return mostLikes
+    }
+    else {
+      return formatBlog(mostLikes)  
+    } */
+    
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
+
