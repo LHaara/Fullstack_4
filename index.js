12a24134f89f670b47d36814291c2f3d83860186
Lauri Haara <lauri.haara@gmail.com>
2018-11-23 14:44:30 +0200

    4_15

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ const morgan = require('morgan')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
+const usersRouter = require('./controllers/users')
 
-mongoose
+
+
+mongoose.connect(config.mongoUrl, { useNewUrlParser: true})
+
+//mongoose.Promise = global.Promise
+
+/* mongoose
   .connect(config.mongoUrl, { useNewUrlParser: true})
   .then( () => {
     console.log('connected to database', config.mongoUrl)
   })
   .catch( err => {
     console.log(err)
-  })
+  }) */
 
 /* morgan.token('data', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :data :status :res[content-length] - :response-time ms')) */
@@ -24,16 +31,14 @@ app.use(morgan(':method :url :data :status :res[content-length] - :response-time
 app.use(cors())
 app.use(bodyParser.json())
 app.use(middleware.logger)
+
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
 app.use(middleware.error)
 //app.use(express.static('build'))
 
 
-
-/* const PORT = config.port
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-}) */
 const server = http.createServer(app)
 
 server.listen(config.port, () => {
